dc3456ff2317044f91ec29e68521ce681b507fea
thalesleite <thalesaleite@gmail.com>
2020-03-30 14:32:48 +0100

    added port for heruko

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { erros } = require('celebrate');
+//const { erros } = require('celebrate');
 const routes = require('./routes');
 
 const app = express();
